cb57aff14c6f9e8c5d7557e5e11c8f4a1aae3289
pushsla <push2001sla@gmail.com>
2020-05-31 02:57:00 +0500

    (Feature) Try to add bot reaction to attachments

--- echobot/Handlers/MessageHandler.cs
+++ echobot/Handlers/MessageHandler.cs
@@ -11,12 +11,23 @@ namespace echobot.Handlers
     {
         public static void HandleNewMessage(VkObject objectMessage, IVkApi vkApi)
         {
+            string text = objectMessage.Message.Text;
             vkApi.Messages.SetActivity(vkApi.UserId.ToString(), MessageActivityType.Typing, objectMessage.Message.PeerId);
             Thread.Sleep(1000);
+
+            if (objectMessage.Message.Attachments.Length > 0)
+            {
+                text += "\nА еще ты прикрепил ";
+                foreach (var atch in objectMessage.Message.Attachments)
+                {
+                    text += $"{atch.Type}, ";
+                }
+            }
+
             vkApi.Messages.Send(new MessagesSendParams{ 
                 RandomId = new DateTime().Millisecond,
                 PeerId = objectMessage.Message.PeerId,
-                Message = objectMessage.Message.Text
+                Message = text
             });
         }
     }
