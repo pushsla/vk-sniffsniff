0d1c370e56740fd54d5b6301a7ced6f4527148fd
pushsla <push2001sla@gmail.com>
2020-05-31 01:16:13 +0500

    (Feature) try to humanize messaging process

--- echobot/Handlers/MessageHandler.cs
+++ echobot/Handlers/MessageHandler.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading;
 using VkNet.Abstractions;
+using VkNet.Enums.SafetyEnums;
 using VkNet.Model.RequestParams;
 
 namespace echobot.Handlers
@@ -8,6 +10,8 @@ namespace echobot.Handlers
     {
         public static void HandleNewMessage(sniff.Object objectMessage, IVkApi vkApi)
         {
+            vkApi.Messages.SetActivity(vkApi.UserId.ToString(), MessageActivityType.Typing, objectMessage.Message.PeerId);
+            Thread.Sleep(1000);
             vkApi.Messages.Send(new MessagesSendParams{ 
                 RandomId = new DateTime().Millisecond,
                 PeerId = objectMessage.Message.PeerId,
